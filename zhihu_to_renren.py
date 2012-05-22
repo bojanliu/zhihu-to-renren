6b2efe47791360a7cfd1d024d8144c1ef1b971bf
BojanLiu <lllhhw@gmail.com>
2012-05-22 14:55:55 +0800

    抓取知乎页面数据部分，用正则进行了重写。之前用pyquery解决不了抓取正文的问题。

--- zhihu_to_renren.py
+++ zhihu_to_renren.py
@@ -1,64 +1,117 @@
-# -*- coding:utf-8 -*-
-#filename:zhihu_to_renren.py
-#自动登录知乎网站,抓取explore页面,自动登录人人网发表日志
+#coding:utf-8
+#file_name:zhihu_to_renren.py
+#time:20120518
+
 import cookielib
 import urllib2
 import urllib
 from pyquery import PyQuery as pq
+import re
 
-cj=cookielib.CookieJar()#建立Cookie实例
-cookies=urllib2.HTTPCookieProcessor(cj)
-opener=urllib2.build_opener(cookies)#建立opener与cookie关联
-urllib2.install_opener(opener)
-post_data=urllib.urlencode({
-            'email':'******@gmail.com',
-            'password':'******',
-            })#登录知乎的表单数据
-headers={'User-Agent':'IE',}#伪装成浏览器访问
-req=urllib2.Request(url='http://www.zhihu.com/login',#zhihu的登录url
-                    data=post_data,
-                    headers=headers,
-                    )
-result=urllib2.urlopen(req)#发送http请求
-#--------------------------------------------------------------------#
-html_source=pq(url='http://www.zhihu.com/explore')#抓取知乎热门问答页面内容
-html_source=html_source(".xvq.xaq").html()#根据类名得到排名第一的问答块HTML内容
-result=pq(html_source)
-title=result('.xmq').text() #提取出题目
-content=result('.xbq').html()#提取出回答内容，保留html标签，以保证日志格式
-link=result('.xmq').find('a').attr('href')#提取出原文链接
-author=result('.xrq').find('img').attr('title')#提取出作者
-#--------------------------------------------------------------------#
-if all((html_source,title,content,link,author,),):#判断是否有值为None
-    link='www.zhihu.com'+link
-    post_data=urllib.urlencode({ 
-                'email':'******',
-                'password':'******',
-                'domain':'renren.com',
-                })#登录人人的表单数据
-    req=urllib2.Request(url='http://www.renren.com/PLogin.do',#人人的登录url
-                        data=post_data,
-                        headers=headers,
-                        )
-    result=urllib2.urlopen(req)#发送http请求
-    html_source=pq(url='http://blog.renren.com/NewEntry.do')
-    post_id=html_source('#postFormId').attr('value')#获得postFormId值
+
+def setup_cookie():
+    cj=cookielib.CookieJar()
+    cookies=urllib2.HTTPCookieProcessor(cj)
+    opener=urllib2.build_opener(cookies)
+    urllib2.install_opener(opener)
+
+def auto_post(post_url, post_data):
+    header_info={'User-Agent':'IE',}
+    req=urllib2.Request(
+        url=post_url,
+        headers=header_info,
+        data=post_data,
+        )
+    result=urllib2.urlopen(req)
+
+#登录知乎
+def login_zhihu():
+    login_email='********@gmail.com'
+    login_password='********'
+    login_url='http://www.zhihu.com/login' 
     post_data=urllib.urlencode({
-        'title':'知乎每日热门问答——%s'%title.encode("utf-8"),#日志标题
-        'body':"<p>回答者：%s</p>%s<p>原地址：%s</p>"#日志内容
-            %(author.encode("utf-8"),
-              content.encode("utf-8"),
-              link.encode("utf-8"),),
+        'email':login_email,
+        'password':login_password,
+        })
+    auto_post(login_url,post_data)
+
+#解析知乎
+def parse_zhihu():
+    #待解析页面——知乎“发现”页面
+    target_url='http://www.zhihu.com/explore'
+    
+    data=urllib2.urlopen(target_url).read()
+    #得到“发现页面”首篇文章链接
+    link_1=re.findall(r'/question/(\d{8})/',data)
+    link_2=re.findall(r"""/answer/(\d{8})">""",data)
+    link='http://www.zhihu.com/question/%s/answer/%s'%(link_1[0],link_2[0])
+
+    data=urllib2.urlopen(link).read()
+    
+    #得到文章正文，带HTML标签的。
+    p=re.compile(r"""data-action="/answer/content">(.*?)</div>""",re.S)
+    content=p.findall(data)
+
+    #得到文章标题
+    title=re.findall(r"""<a href="/question/\d{8}">(.*?)</a>""",data)
+
+    #得到文章回答者
+    author=re.findall(r"""<a data-tip=".*?" href="/people/.*?">(.*?)</a>""",data)
+
+    #得到回答者自我描述
+    about=re.findall(r"""<strong title=".*?" class=".*?">(.*?)</strong>""",data)
+
+    return [title, link, content, author, about]
+
+#登录人人
+def login_renren():
+    login_renren_email='********'
+    login_renren_passwd='*******'
+    login_renren_url='http://www.renren.com/PLogin.do'
+    domain='renren.com'
+    post_data=urllib.urlencode({
+        'email':login_renren_email,
+        'password':login_renren_passwd,
+        'domain':domain,
+        })
+    auto_post(login_renren_url,post_data)
+
+#发表人人日志
+def post_renren_blog():
+    post_blog_url='http://blog.renren.com/NewEntry.do'
+
+    #解析得出post_id这个动态生成的值
+    html_source=pq(url=post_blog_url)
+    post_id=html_source('#postFormId').attr('value')
+
+    #日志内容即来自解析知乎得到的文章数据
+    zhihu_data=parse_zhihu()
+
+    #发表人人日志提交的数据
+    post_data=urllib.urlencode({
+        'title':'知乎每日热门问答——%s'%zhihu_data[0][0],
+        'body':"""<p>Posted by zhihu2renren Robot——每天十点，分享知乎精彩问答！</p>
+                  <p>原文地址：%s</p>
+                  <p>回答者：%s</p>
+                  <p>回答者简介：%s</p>%s
+               """%(zhihu_data[3][0],
+                    zhihu_data[1],
+                    zhihu_data[4][0],
+                    zhihu_data[2][0],),
         'categoryId':'0',
         'blogControl':'99',
         'postFormId':post_id,
         'relative_optype':'saveDraft',
-                            })
-    req=urllib2.Request(url='http://blog.renren.com/NewEntry.do',#发表日志url
-                         data=post_data,
-                         headers=headers,)
-    result=urllib2.urlopen(req)
-    print 'it is over!'
-else:
-    print 'sorry, but there is an error!'
+        })
+    auto_post(post_blog_url,post_data)
+    
 
+def main():
+    setup_cookie()
+    login_zhihu()
+    login_renren()
+    post_renren_blog()
+    print 'ok'
+
+if __name__=='__main__':
+    main()
